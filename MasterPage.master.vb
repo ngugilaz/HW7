58133d0bf709e27e5a53082610bb11987f464206
Lazarus <ngugilaz@gmail.com>
2014-04-03 20:41:33 -0500

    modified masterpage and added background image

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -1,5 +1,15 @@
 ﻿
 Partial Class MasterPage
-    Inherits System.Web.UI.Page
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Lbl_date.Text = DateTime.Now.ToLongDateString
+
+        Lbl_year.text = DateTime.Now.Year
+    End Sub
+
+
 
 End Class
+
